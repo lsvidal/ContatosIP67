d0afff1ca1f22ebe1af2a0a7dac414c8ebd03089
Leonardo Vidal <leonardo73vidal@gmail.com>
2012-12-04 12:26:46 -0200

    Initial Commit

---
+++ ContatosIP67/CMPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CMPAppDelegate.h
+//  ContatosIP67
+//
+//  Created by ios3109 on 04/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CMPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ContatosIP67/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ContatosIP67
+//
+//  Created by ios3109 on 04/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CMPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CMPAppDelegate class]));
+    }
+}
