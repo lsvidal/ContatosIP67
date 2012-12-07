2a713cae51d26a30dde4900ad71ebb5d04c5d8a2
Leonardo Vidal <leonardo73vidal@gmail.com>
2012-12-07 10:13:02 -0200

    Classe contato implementa o protocolo NSCoding.

--- ContatosIP67/Contato.m
+++ ContatosIP67/Contato.m
@@ -12,4 +12,26 @@
 
 @synthesize nome, email, endereco, telefone, site, idade;
 
+- (void)encodeWithCoder:(NSCoder *)aCoder {
+    [aCoder encodeObject:nome forKey:@"nome"];
+    [aCoder encodeObject:email forKey:@"email"];
+    [aCoder encodeObject:endereco forKey:@"endereco"];
+    [aCoder encodeObject:telefone forKey:@"telefone"];
+    [aCoder encodeObject:site forKey:@"site"];
+    [aCoder encodeObject:idade forKey:@"idade"];
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder {
+    self = [super init];
+    if (self) {
+        self.nome = [aDecoder decodeObjectForKey:@"nome"];
+        self.email = [aDecoder decodeObjectForKey:@"email"];
+        self.endereco = [aDecoder decodeObjectForKey:@"endereco"];
+        self.telefone = [aDecoder decodeObjectForKey:@"telefone"];
+        self.site = [aDecoder decodeObjectForKey:@"site"];
+        self.idade = [aDecoder decodeObjectForKey:@"idade"];
+    }
+    return self;
+}
+
 @end
